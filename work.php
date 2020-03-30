7150770813536c7a8bd517b366bd1076c65bc8b7
jnc83 <jnc83@cornell.edu>
2020-03-30 15:48:47 -0400

    work page uploaded and font added

--- work.php
+++ work.php
@@ -18,21 +18,33 @@ include("includes/header.php");
   <div class = "workImageRow">
     <figure>
       <div class = "eachWorkPic">
-        <img class="workPagePics" src = "images/GRTworkPageTransparent.png" alt = "System Access Review Tool Logo"/>
+        <img class="workPagePics transparent" src = "images/GRTworkPageTransparent.png" alt = "System Access Review Tool Logo"/>
         <img class="workPagePics imageTop" src = "images/GRTworkPage.png" alt = "System Access Review Tool Logo"/>
       </div>
+      <figcaption id="caption1">
+        Access Review Tool
+        <p> ;lakjsdf;lkjasdl;kjf;lkajsdf;ljasd a;lksdjfl;kjas dkf asdkf;ask djf ;lsajd;flj as;dlkj f </p>
+      </figcaption>
     </figure>
     <figure>
       <div class = "eachWorkPic">
-        <img class="workPagePics" src = "images/APASworkPageTransparent.png" alt = "Any Person Any Study Logo"/>
+        <img class="workPagePics transparent" src = "images/APASworkPageTransparent.png" alt = "Any Person Any Study Logo"/>
         <img class="workPagePics imageTop" src = "images/APASworkPage.png" alt = "Any Person Any Study Logo"/>
       </div>
+      <figcaption id="caption2">
+        Any Person Any Study
+        <p> ;lakjsdf;lkjasdl;kjf;lkajsdf;ljasd a;lksdjfl;kjas dkf asdkf;ask djf ;lsajd;flj as;dlkj f </p>
+      </figcaption>
     </figure>
     <figure>
       <div class = "eachWorkPic">
-        <img class="workPagePics" src = "images/CWRworkPageTransparent2.png" alt = "Cornell Welcomes Refugees Logo"/>
+        <img class="workPagePics transparent" src = "images/CWRworkPageTransparent2.png" alt = "Cornell Welcomes Refugees Logo"/>
         <img class="workPagePics imageTop" src = "images/CWRworkPage.png" alt = "Cornell Welcomes Refugees Logo"/>
       </div>
+      <figcaption id="caption3">
+        Cornell Welcomes Refugees Website
+        <p> ;lakjsdf;lkjasdl;kjf;lkajsdf;ljasd a;lksdjfl;kjas dkf asdkf;ask djf ;lsajd;flj as;dlkj f </p>
+      </figcaption>
     </figure>
   </div>
 
